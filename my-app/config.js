93af198f7b3bd2db1b28920fd2ed9655aafb6333
kevdanone <kevlevagabon@hotmail.com>
2021-07-09 14:14:37 +0200

    edit folio

---
+++ my-app/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteTitle: 'Kevin Folio', // <title>
+  manifestName: 'SolidState',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'My name is Kevin Lauro',
+  subHeading: 'Web Developer.',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/kevdanone',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/kevdanone',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/kevin.lauro.18',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:kevin.lauro@yahoo.com',
+    },
+  ],
+  phone: '0681871625',
+  address: '3 rue du commandant Girardot, 70000 Vesoul',
+};
